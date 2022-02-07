26837efce9c7c131509b7071be6967dce6bfb260
Doug Revell <doug.revelljr@gmail.com>
2022-02-07 15:31:49 -0500

    Add Edit Functionality

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using RestaurantRaterMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -64,5 +65,36 @@ namespace RestaurantRaterMVC.Controllers
             return RedirectToAction("Index");
         }
 
+        //GET: Restaurant/Edit/{id}
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Restaurant restaurant = _restaurantDbContext.Restaurants.Find(id);
+            if (restaurant == null)
+            {
+                return HttpNotFound();
+            }
+            return View(restaurant);
+        }
+
+
+        //POST: Restaurant/Edit/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                _restaurantDbContext.Entry(restaurant).State = EntityState.Modified;
+                _restaurantDbContext.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(restaurant);
+        }
     }
+
 }
